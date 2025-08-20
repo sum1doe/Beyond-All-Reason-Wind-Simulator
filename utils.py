975a371a475b61781364f6c5f9bd6e6fcb63abe9
sum1doe <30567064+sum1doe@users.noreply.github.com>
2025-08-20 16:28:27 +0200

    removed unnecessary clamp call.

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ def clamp(value, min_value, max_value):
 def smooth_step(edge1, edge2, value):
     v = clamp(value, edge1, edge2)
     x = (v - edge1) / (edge2 - edge1)
-    t = clamp(x, 0.0, 1.0)
+    return (x * x * (3.0 - 2.0 * x))
+    t = x #clamp(x, 0.0, 1.0)
     res = (t * t * (3.0 - 2.0 * t))
     return res
 
